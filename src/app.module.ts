cf2f0e865039fc448d81e290cf8dfb6891cf33eb
hakimiomari <hakimikamranullah@gmail.com>
2024-12-18 14:51:30 +0430

    create user controller and service

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
+import { VehiclesModule } from './vehicles/vehicles.module';
 
 @Module({
-  imports: [],
+  imports: [UsersModule, VehiclesModule],
   controllers: [AppController],
   providers: [AppService],
 })
