429fb5d8c57dafd19ca8f016eca25ec5b93d9869
Kamranullah Hakimi <hakimikamranullah@gmail.com>
2024-12-18 18:14:18 +0430

    revomve vehicle controller import;

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,9 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
-import { VehiclesModule } from './vehicles/vehicles.module';
 
 @Module({
-  imports: [UsersModule, VehiclesModule],
+  imports: [UsersModule],
   controllers: [AppController],
   providers: [AppService],
 })
