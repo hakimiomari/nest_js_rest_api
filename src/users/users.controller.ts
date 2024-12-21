df49156a6fa5e250205056a32c94a62465e84f4b
hakimiomari <hakimikamranullah@gmail.com>
2024-12-21 11:48:03 +0430

    install and setup swagger

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,9 +1,18 @@
-import { Controller, Get, Query } from '@nestjs/common';
+import { Controller, Get, Param, Query } from '@nestjs/common';
+import { ApiTags, ApiOperation } from '@nestjs/swagger';
 
+@ApiTags('Users')
 @Controller('users')
 export class UsersController {
   @Get()
+  @ApiOperation({ summary: 'Get all users' })
   findAll(@Query('role') role: 'INTERN' | 'ENGINEER' | 'ADMIN') {
     return { result: true };
+  }
+
+  @Get(':id')
+  @ApiOperation({ summary: 'find specific user' })
+  findOne(@Param('id') id: string) {
+    return { result: true, id };
   }
 }
