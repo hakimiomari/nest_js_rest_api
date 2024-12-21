8b05616cdc82dafb2605e485eecd9ddcce16c92a
hakimiomari <hakimikamranullah@gmail.com>
2024-12-21 11:10:28 +0430

    add findAll function

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,9 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Query } from '@nestjs/common';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+  @Get()
+  findAll(@Query('role') role: 'INTERN' | 'ENGINEER' | 'ADMIN') {
+    return { result: true };
+  }
+}
