255130c1e6dc31263194fea9c4c370bce9334d0b
Kamranullah Hakimi <hakimikamranullah@gmail.com>
2025-03-01 04:47:10 +0430

    add guard

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,18 +1,28 @@
-import { Controller, Get, Header, HostParam, Req, Res } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Header,
+  HostParam,
+  Req,
+  Res,
+  UseGuards,
+} from '@nestjs/common';
 import { AppService } from './app.service';
 import { Response } from 'express';
+import { AuthGuard } from './guard/auth.guard';
 
 @Controller() // This decorator marks the class as a NestJS controller
 export class AppController {
   constructor(private readonly appService: AppService) {} // Dependency injection of AppService
 
   @Get() // This decorator creates a GET endpoint
+  @UseGuards(AuthGuard)
   getHello(): string {
     return this.appService.getHello(); // Calls the getHello method from AppService
   }
   @Get('all')
-  findAll(@HostParam('subdomain') subdomain: string){
-    console.log(subdomain)
+  findAll(@HostParam('subdomain') subdomain: string) {
+    console.log(subdomain);
     return 'This action returns all cats';
   }
 
@@ -22,6 +32,6 @@ export class AppController {
     res.setHeader('Cache-Control', 'no-store');
     res.setHeader('Pragma', 'no-cache');
     res.setHeader('Expires', '0');
-    res.send({message:'This is a wildcard route'});
+    res.send({ message: 'This is a wildcard route' });
   }
 }
--- src/main.ts
+++ src/main.ts
@@ -16,6 +16,6 @@ async function bootstrap() {
 
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api-docs', app, document);
-  await app.listen(process.env.PORT ?? 8000);
+  await app.listen(process.env.PORT ?? 8002);
 }
 bootstrap();
