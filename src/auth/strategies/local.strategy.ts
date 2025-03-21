be1e27d4737d27ec4476d8760c72c999f64a1e5b
Kamranullah Hakimi <hakimikamranullah@gmail.com>
2025-03-21 23:58:15 +0430

    add local strategy

--- src/auth/strategies/local.strategy.ts
+++ src/auth/strategies/local.strategy.ts
@@ -1,6 +1,7 @@
 import { PassportStrategy } from '@nestjs/passport';
 import { Strategy } from 'passport-local';
 import { AuthService } from '../auth.service';
+import { UnauthorizedException } from '@nestjs/common';
 
 export class LocalStrategy extends PassportStrategy(Strategy) {
   constructor(private authService: AuthService) {
@@ -8,6 +9,11 @@ export class LocalStrategy extends PassportStrategy(Strategy) {
   }
 
   validate(username: string, password: string): Promise<any> {
-    return this.authService.validateUser(username, password);
+    const user = this.authService.validateUser(username, password);
+
+    if (!user) {
+      throw new UnauthorizedException();
+    }
+    return user;
   }
 }
